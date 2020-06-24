5ff3ba5db7d94c855bf1039ffeeeda5f06b9e33f
youngchang8660 <youngchang8660@gmail.com>
2020-06-24 10:13:06 -0600

    second commit

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { TableCell, TableRow } from "@material-ui/core";
-import { connect } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
